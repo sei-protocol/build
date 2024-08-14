06e9b4ca4bc556d175010fcb85d14ec6bd48f246
Wojciech Małota-Wójcik <wojciech@seinetwork.io>
2024-08-14 16:19:56 +0200

    Generate code from protobufs (#16)

--- cmd/builder/build.go
+++ cmd/builder/build.go
@@ -7,10 +7,13 @@ import (
 
 	"github.com/sei-protocol/build/pkg/tools"
 	"github.com/sei-protocol/build/pkg/tools/golang"
+	"github.com/sei-protocol/build/pkg/tools/protobuf"
 	"github.com/sei-protocol/build/pkg/tools/rust"
 )
 
 func buildGo(ctx context.Context, deps build.DepsFunc) error {
+	deps(generateGo)
+
 	return golang.Build(ctx, deps, golang.BuildConfig{
 		Platform:      tools.PlatformLocal,
 		PackagePath:   "testapps/golang",
@@ -25,4 +28,16 @@ func buildRust(ctx context.Context, deps build.DepsFunc) error {
 		Binary:        "rust",
 		BinOutputPath: "bin/test-rust",
 	})
+}
+
+func generateGo(ctx context.Context, deps build.DepsFunc) error {
+	const (
+		protoDir = "testapps/golang/proto"
+		outDir   = "testapps/golang/generated"
+	)
+	if err := protobuf.GenerateGo(ctx, deps, protoDir, outDir); err != nil {
+		return err
+	}
+
+	return protobuf.GenerateGoGRPC(ctx, deps, protoDir, outDir)
 }
