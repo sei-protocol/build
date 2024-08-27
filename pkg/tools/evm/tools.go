38c6c0ab9e0f1e142d7337fee5560f2fc320b479
Steven Landers <steven.landers@gmail.com>
2024-08-27 13:24:56 -0400

    update tool references (#39)

--- pkg/tools/evm/tools.go
+++ pkg/tools/evm/tools.go
@@ -1,6 +1,10 @@
 package evm
 
 import (
+	"context"
+
+	"github.com/outofforest/build"
+
 	"github.com/sei-protocol/build/pkg/tools"
 )
 
@@ -17,33 +21,38 @@ var t = []tools.Tool{
 		Sources: tools.Sources{
 			tools.PlatformLinuxAMD64: {
 				URL:  "https://github.com/foundry-rs/foundry/releases/download/nightly-2b1f8d6dd90f9790faf0528e05e60e573a7569ce/foundry_nightly_linux_amd64.tar.gz", //nolint:lll
-				Hash: "sha256:2c75c62fab2a521938fd2a5eec6e97f9718eb0b6802852f54f1d886100fe8eb0",
+				Hash: "sha256:2c75c62fab2a521938fd2a5eec6e97f9718eb0b6802852f54f1d886100fe8eb0",                                                                     //nolint:lll
 				Links: map[string]string{
-					"bin/anvil":   "foundry",
-					"bin/foundry": "anvil",
-					"bin/cast":    "cast",
+					"bin/anvil": "anvil",
+					"bin/forge": "forge",
+					"bin/cast":  "cast",
 				},
 			},
 			tools.PlatformDarwinAMD64: {
 				URL:  "https://github.com/foundry-rs/foundry/releases/download/nightly-2b1f8d6dd90f9790faf0528e05e60e573a7569ce/foundry_nightly_darwin_amd64.tar.gz", //nolint:lll
-				Hash: "sha256:cf853e416cf9358174bf4fcf603b5c263aed456842b9c78661c4d77654133b7a",
+				Hash: "sha256:cf853e416cf9358174bf4fcf603b5c263aed456842b9c78661c4d77654133b7a",                                                                      //nolint:lll
 				Links: map[string]string{
-					"bin/anvil":   "foundry",
-					"bin/foundry": "anvil",
-					"bin/cast":    "cast",
+					"bin/anvil": "anvil",
+					"bin/forge": "forge",
+					"bin/cast":  "cast",
 				},
 			},
 			tools.PlatformDarwinARM64: {
 				URL:  "https://github.com/foundry-rs/foundry/releases/download/nightly-2b1f8d6dd90f9790faf0528e05e60e573a7569ce/foundry_nightly_darwin_arm64.tar.gz", //nolint:lll
-				Hash: "sha256:af157f6daac33bb4b955875e777c52d7d022e6471ed2bf1cddba9869ed5707f0",
+				Hash: "sha256:af157f6daac33bb4b955875e777c52d7d022e6471ed2bf1cddba9869ed5707f0",                                                                      //nolint:lll
 				Links: map[string]string{
-					"bin/anvil":   "foundry",
-					"bin/foundry": "anvil",
-					"bin/cast":    "cast",
+					"bin/anvil": "anvil",
+					"bin/forge": "forge",
+					"bin/cast":  "cast",
 				},
 			},
 		},
 	},
+}
+
+// EnsureFoundry ensures that foundry is available.
+func EnsureFoundry(ctx context.Context, _ build.DepsFunc) error {
+	return tools.Ensure(ctx, Foundry, tools.PlatformLocal)
 }
 
 func init() {
